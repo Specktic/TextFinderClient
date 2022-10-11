15566c98e581e8706972889727289ebe9868df47
Specktic <111095859+Specktic@users.noreply.github.com>
2022-10-11 13:46:51 -0600

    - Rename classes - Add client-view.fxml file - Add test variants of The Road Not Taken poem

---
+++ src/main/java/com/example/textfinderclient/ClientApplication.java
@@ -0,0 +1,15 @@
+package com.example.textfinderclient;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ClientApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/textfinderclient/ClientController.java
@@ -0,0 +1,5 @@
+package com.example.textfinderclient;
+
+public class ClientController {
+
+}
